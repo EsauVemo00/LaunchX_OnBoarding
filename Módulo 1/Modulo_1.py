1ce71fd5c0db2e63b09edbd166409e06669e6e85
Esaú Vela <66577199+EsauVemo00@users.noreply.github.com>
2022-02-13 23:04:32 -0600

    Add: Módulo 1
    Agregué el módulo con la kata #1

---
+++ Módulo 1/Modulo_1.py
@@ -0,0 +1,27 @@
+# Modulo_1.py
+sum=1+2 #3
+print(sum)
+print('Hola desde la consola')
+product=sum*2
+print(product)
+# Declaramos la variable
+distancia_a_alpha_centauri=4.367
+# Descubrimos su tipo de dato con (Type)
+tipo=type(distancia_a_alpha_centauri)  
+print(tipo) 
+
+from datetime import date 
+print(date.today())
+# Ejecutar mensaje con fecha
+print('Hoy es: '+str(date.today()))
+
+# Entrada de usuario
+print('Bienvenido al programa de Bienvenida')
+name=input('Introduzca su nombre: ')
+print('Saludos: '+name)
+
+# Trabajar con números
+print('Calculadora')
+primernum=input('Primer número: ')
+segundonum=input('Segundo número: ')
+print(int(primernum)+int(segundonum))
